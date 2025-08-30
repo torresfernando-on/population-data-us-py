fe2c87fe99875a2c8e21482a6b2c3373d1ccc083
torresfernando-on <torresfernando1419@gmail.com>
2025-08-30 16:12:17 -0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# first test with python in Github Web
 
+# Libraries
+import pandas as pd
+import matplotlib as plt
+
+url = "https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv"
+
+df = pd.read_csv(url)
+
+print(df.head())
